01199451003aa00e51644d2510ef7cfa7ea7c674
Ahsan Noor <ahsan.noor@auxilium.digital>
2022-05-19 14:20:08 +0500

    Created Models for ShoppingCart

---
+++ Mango.Services.ShoppingCartAPI/Models/CartHeader.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mango.Services.ShoppingCartAPI.Models
+{
+    public class CartHeader
+    {
+        [Key]
+        public int CartHeaderid { get; set; }
+        public string Userid { get; set; }
+        public string CouponCode { get; set; }
+    }
+}
