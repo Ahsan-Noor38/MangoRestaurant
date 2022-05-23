b3c0a2f931f325fabcd7f327aeacb39c5e680dee
Ahsan Noor <ahsan.noor@auxilium.digital>
2022-05-23 13:27:20 +0500

    Create DTOs in Main Project for shopping cart API and also Configured

---
+++ Mango.Web/Models/CartHeaderDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Mango.Web.Models
+{
+    public class CartHeaderDTO
+    {
+        public int CartHeaderid { get; set; }
+        public string Userid { get; set; }
+        public string CouponCode { get; set; }
+        public double OrderTotal { get; set; }
+    }
+}
