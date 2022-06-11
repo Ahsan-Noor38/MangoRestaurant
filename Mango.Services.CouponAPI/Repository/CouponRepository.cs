0aefb35627910109bbcc95153c5c69a291758df9
Ahsan Noor <ahsan.noor@auxilium.digital>
2022-06-11 15:44:06 +0500

    Added Model DTO DbContext and Repository for couponAPI

---
+++ Mango.Services.CouponAPI/Repository/CouponRepository.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Mango.Services.CouponAPI.DbContexts;
+using Mango.Services.CouponAPI.Models.DTOs;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace Mango.Services.CouponAPI.Repository
+{
+    public class CouponRepository : ICouponRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+        protected IMapper _mapper;
+        public CouponRepository(ApplicationDbContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+        public async Task<CouponDTO> GetCouponByCode(string couponCode)
+        {
+            var couponFromDb = await _dbContext.Coupons.FirstOrDefaultAsync(c => c.CouponCode == couponCode);
+            return _mapper.Map<CouponDTO>(couponFromDb);
+        }
+    }
+}
