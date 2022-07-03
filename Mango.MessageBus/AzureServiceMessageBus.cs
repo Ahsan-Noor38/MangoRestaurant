0f3aa6b98287bffd30fa9379ffb7db090fdc28e0
Ahsan Noor <ahsan.noor@auxilium.digital>
2022-07-03 18:57:07 +0500

    added Service bus Interface and sender implemetation 1 & 2

---
+++ Mango.MessageBus/AzureServiceMessageBus.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Azure.ServiceBus;
+using Microsoft.Azure.ServiceBus.Core;
+using Newtonsoft.Json;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mango.MessageBus
+{
+    public class AzureServiceMessageBus : IMessageBus
+    {
+        private string connectionString = "Endpoint=sb://ahsanrestaurant.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=vxikbmfln02UfR31iUu6UqTg1oYT0o3h5GPKPTV9oyI=";
+        public async Task PublishMessage(BaseMessage message, string topicName)
+        {
+            ISenderClient senderClient = new TopicClient(connectionString, topicName);
+
+            var jsonMessage = JsonConvert.SerializeObject(message);
+            var finalMessage = new Message(Encoding.UTF8.GetBytes(jsonMessage))
+            {
+                CorrelationId = Guid.NewGuid().ToString(),
+            };
+
+            await senderClient.SendAsync(finalMessage);
+            await senderClient.CloseAsync();
+        }
+    }
+}
