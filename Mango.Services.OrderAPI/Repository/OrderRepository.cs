43563b9c8f09ed8aafc2a85715902fdc7aed00b9
Ahsan Noor <ahsan.noor@auxilium.digital>
2022-07-04 14:32:17 +0500

    implemented order Repository interface and its implementation

---
+++ Mango.Services.OrderAPI/Repository/OrderRepository.cs
@@ -0,0 +1,36 @@
+﻿using Mango.Services.OrderAPI.DbContexts;
+using Mango.Services.OrderAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace Mango.Services.OrderAPI.Repository
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private readonly DbContextOptions<ApplicationDbContext> _dbContextOptions;
+
+        public OrderRepository(DbContextOptions<ApplicationDbContext> dbContextOptions)
+        {
+            _dbContextOptions = dbContextOptions;
+        }
+
+        public async Task<bool> AddOrder(OrderHeader orderHeader)
+        {
+            await using var _dbContext = new ApplicationDbContext(_dbContextOptions);
+            _dbContext.OrderHeaders.Add(orderHeader);
+            await _dbContext.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task UpdateOrderPaymentStatus(int orderHeaderId, bool isPaid)
+        {
+            await using var _dbContext = new ApplicationDbContext(_dbContextOptions);
+            var orderHeaderFromDb = await _dbContext.OrderHeaders.FirstOrDefaultAsync(o => o.OrderHeaderId == orderHeaderId);
+            if (orderHeaderFromDb != null)
+            {
+                orderHeaderFromDb.PaymentStatus = isPaid;
+                await _dbContext.SaveChangesAsync();
+            }
+        }
+    }
+}
