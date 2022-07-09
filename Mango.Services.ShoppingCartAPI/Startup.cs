0c7609a4a57af8cc75b402854a5b238a2c3e1139
Ahsan Noor <ahsan.noor@auxilium.digital>
2022-07-09 23:00:14 +0500

    Configure StartUp File for Coupon repository for sync - 2

--- Mango.Services.ShoppingCartAPI/Startup.cs
+++ Mango.Services.ShoppingCartAPI/Startup.cs
@@ -39,8 +39,10 @@ namespace Mango.Services.ShoppingCartAPI
             services.AddSingleton(mapper);
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             services.AddScoped<ICartRepository, CartRepository>();
+            services.AddScoped<ICouponRepository, CouponRepository>();
             services.AddSingleton<IMessageBus, AzureServiceMessageBus>();
             services.AddControllers();
+            services.AddHttpClient<ICouponRepository, CouponRepository>(u => u.BaseAddress = new Uri(Configuration["ServiceUrls:CouponAPI"]));
 
             services.AddAuthentication("Bearer")
                 .AddJwtBearer("Bearer", options =>
